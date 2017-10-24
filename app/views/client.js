217a3019c5c1422a1f2ad019ddca602d91d27bce
Jan Baykara <jan@baykara.co.uk>
2017-10-24 11:47:53 +0200

    Persist notification permission state with cookies

--- app/views/client.js
+++ app/views/client.js
@@ -1,3 +1,7 @@
+/*! js-cookie v2.1.4 | MIT */
+
+!function(a){var b=!1;if("function"==typeof define&&define.amd&&(define(a),b=!0),"object"==typeof exports&&(module.exports=a(),b=!0),!b){var c=window.Cookies,d=window.Cookies=a();d.noConflict=function(){return window.Cookies=c,d}}}(function(){function a(){for(var a=0,b={};a<arguments.length;a++){var c=arguments[a];for(var d in c)b[d]=c[d]}return b}function b(c){function d(b,e,f){var g;if("undefined"!=typeof document){if(arguments.length>1){if(f=a({path:"/"},d.defaults,f),"number"==typeof f.expires){var h=new Date;h.setMilliseconds(h.getMilliseconds()+864e5*f.expires),f.expires=h}f.expires=f.expires?f.expires.toUTCString():"";try{g=JSON.stringify(e),/^[\{\[]/.test(g)&&(e=g)}catch(p){}e=c.write?c.write(e,b):encodeURIComponent(e+"").replace(/%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g,decodeURIComponent),b=encodeURIComponent(b+""),b=b.replace(/%(23|24|26|2B|5E|60|7C)/g,decodeURIComponent),b=b.replace(/[\(\)]/g,escape);var i="";for(var j in f)f[j]&&(i+="; "+j,!0!==f[j]&&(i+="="+f[j]));return document.cookie=b+"="+e+i}b||(g={});for(var k=document.cookie?document.cookie.split("; "):[],l=0;l<k.length;l++){var m=k[l].split("="),n=m.slice(1).join("=");'"'===n.charAt(0)&&(n=n.slice(1,-1));try{var o=m[0].replace(/(%[0-9A-Z]{2})+/g,decodeURIComponent);if(n=c.read?c.read(n,o):c(n,o)||n.replace(/(%[0-9A-Z]{2})+/g,decodeURIComponent),this.json)try{n=JSON.parse(n)}catch(p){}if(b===o){g=n;break}b||(g[o]=n)}catch(p){}}return g}}return d.set=d,d.get=function(a){return d.call(d,a)},d.getJSON=function(){return d.apply({json:!0},[].slice.call(arguments))},d.defaults={},d.remove=function(b,c){d(b,"",a(c,{expires:-1}))},d.withConverter=b,d}return b(function(){})});
+
 // Initialize Firebase
 var config = {
   apiKey: "AIzaSyCNBORD9FVS3JPtBmg6DEnUewb_3imJsbI",
@@ -14,8 +18,8 @@ const messaging = firebase.messaging();
 var appState = {
   userID: 1627888800569309, // Demo
   userManagerID: 1627888800569309, // Demo
-  pushEnabled: false,
-  pushToken: null
+  pushEnabled: Boolean(Cookies.get('forgetmenot-push-token')),
+  pushToken: Cookies.get('forgetmenot-push-token')
 }
 
 new Vue({
@@ -86,6 +90,7 @@ function sendTokenToServer(currentToken) {
     console.log(JSON.stringify(x))
     if(!x.error) {
       console.log("Server response!");
+      Cookies.set('forgetmenot-push-token', currentToken);
       appState.pushToken = currentToken;
       appState.pushEnabled = true;
     }
@@ -115,6 +120,7 @@ function notify() {
   .then(x => console.log(JSON.stringify(x)))
 }
 
+// Display notifications when the browser is active (user is clicked on)
 messaging.onMessage(function(notification) {
   console.log('[firebase-messaging-sw.js] Received background message ', notification);
   // Customize notification here
