84e1e4d4ccef7edfac7685620cf3d74fee1192ba
Jeremy Evans <jeremy.n.evans@gmail.com>
2017-11-17 13:13:31 +0000

    Most graphs working

--- app/controller/api.js
+++ app/controller/api.js
@@ -258,13 +258,14 @@ exports.fetchMixpanelData = function(data) {
   const start = data.start,
         end = data.end,
         event = data.event,
-        properties = data.properties
+        properties = data.properties,
+        limit = data.limit
   return new Promise((resolve, reject) => {
-    axios.get('https://4911948a523883a90eba70f3a70d578b@mixpanel.com/api/2.0/segmentation?from_date='+start+'&to_date='+end+'&event='+event+'&on'+properties)
+    axios.get('https://4911948a523883a90eba70f3a70d578b@mixpanel.com/api/2.0/segmentation?from_date='+start+'&to_date='+end+'&event='+event+(properties && properties.length ? '&on=properties["'+properties.join('","')+'"]' : '') + (limit ? '&limit=' + limit : ''))
     .then(function(response) {
       // console.log(response.data);
-      console.log(response.data.data.values);
-      resolve(response.data.data.values)
+      console.log(response.data.data);
+      resolve(response.data.data)
     })
     .catch(function(error) {
       console.log(error);
