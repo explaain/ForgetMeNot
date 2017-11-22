41eb59c15dcfa78e644da5c95ac677b2752ab634
Jeremy Evans <jeremy.n.evans@gmail.com>
2017-11-22 12:30:00 +0000

    Analytics now filters by organisation

--- app/controller/api.js
+++ app/controller/api.js
@@ -300,9 +300,12 @@ exports.fetchMixpanelData = function(data) {
         end = data.end,
         event = data.event,
         properties = data.properties,
-        limit = data.limit
+        limit = data.limit,
+        organisationID = data.organisationID
+  const queryUrl = 'https://4911948a523883a90eba70f3a70d578b@mixpanel.com/api/2.0/segmentation?from_date='+start+'&to_date='+end+'&event='+event + '&where=(properties["organisationID"] == "' + organisationID + '")' + (properties && properties.length ? '&on=properties["'+properties.join('","')+'"]' : '') + (limit ? '&limit=' + limit : '')
+  console.log(queryUrl)
   return new Promise((resolve, reject) => {
-    axios.get('https://4911948a523883a90eba70f3a70d578b@mixpanel.com/api/2.0/segmentation?from_date='+start+'&to_date='+end+'&event='+event+(properties && properties.length ? '&on=properties["'+properties.join('","')+'"]' : '') + (limit ? '&limit=' + limit : ''))
+    axios.get(queryUrl)
     .then(function(response) {
       // console.log(response.data);
       console.log(response.data.data);
